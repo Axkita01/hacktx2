a039cc44857420c12a5ce9ca552a84928aab1b48
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-10-15 22:16:10 -0500

    Update running.py

--- backend/running.py
+++ backend/running.py
@@ -1,5 +1,5 @@
 from gevent.pywsgi import WSGIServer
 from app import app
 
-http_server = WSGIServer(('', 5000), app)
+http_server = WSGIServer(('0.0.0.0:$PORT', 0.0.0.0:$PORT), app)
 http_server.serve_forever()
