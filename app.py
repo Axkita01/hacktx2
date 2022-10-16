1e883786d9932e0746ad7921fc379e257d9df2e6
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-10-16 02:06:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def location():
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    response.headers.add('Access-Control-Allow-Methods', '*')
     return response
 
 if __name__ == "__main__":
