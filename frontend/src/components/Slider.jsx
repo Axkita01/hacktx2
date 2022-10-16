c101a9695cca93cc60f5e248112cce4e7af7ec29
Axkita01 <94666562+Axkita01@users.noreply.github.com>
2022-10-16 00:07:45 -0500

    Delete frontend directory

--- frontend/src/components/Slider.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Rating (props) {
-    selected = props.selected
-    return (
-        
-    );
-}
--- frontend/src/pages/Card.jsx
+++
@@ -1,6 +0,0 @@
-import PlaceCard from '../components/placecard.tsx';
-import Rating from '../components/Slider';
-
-export default function Test() {
-    return <Rating />;
-}
